d85ca7b8cb796e9b41ca7c0e08382d44362a394b
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-10-07 13:19:15 +0530

    app.js add

---
+++ app.js
@@ -0,0 +1,40 @@
+let primearray = [
+  19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+];
+
+function max(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+let maximum = max(primearray);
+console.log(maximum);
+
+function min(arr) {
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+let minimum = min(primearray);
+console.log(minimum);
+
+function sum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+
+const sumarr = sum(primearray);
+console.log(sumarr);
